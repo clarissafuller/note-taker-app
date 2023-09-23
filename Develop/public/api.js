53ebb0d3db1909cc34a5fdcae015f5816863507a
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-22 22:54:22 -0400

    adds post path for api/notes

--- Develop/public/api.js
+++ Develop/public/api.js
@@ -8,4 +8,26 @@ api.get("/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+api.post("/notes", (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    console.log("Response Logged!");
+  } else {
+    console.log("Error in posting note");
+  }
+});
 module.exports = api;
