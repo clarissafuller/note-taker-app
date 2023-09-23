744307a8ecd92af124452ea908e82ee470502f50
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-22 23:16:26 -0400

    fixes local deployment

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const api = require("./Develop/public/api");
+const api = require("./public/api");
 
 const PORT = process.env.PORT || 3001;
 
