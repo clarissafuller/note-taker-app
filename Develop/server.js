afc52c58f48a9db94a6b0f31b82e26fc87be5edc
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-22 22:23:50 -0400

    adds notes.js to try to create routes

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+//ROUTES
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
@@ -17,6 +18,12 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+
+  readFromFile("./db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
